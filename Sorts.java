070dd8545dfa8ee0ca74db56ce6d52603664d1de
sging20 <sging20@stuy.edu>
2020-12-22 21:03:00 -0500

    Currently have bubble and instertion working, now working on Selection

--- Sorts.java
+++ Sorts.java
@@ -1,21 +1,45 @@
+import java.util.Arrays;
 public class Sorts{
   /**Bubble sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data){
-    boolean needswap= true;
     int temp=0;
-    while(needswap== true){
-        needswap= false;
-      for(int i=0; i< data.length-1; i++){
-        if(data[i]> data[i+1]){
-            temp = data[i];
-            data[i]= data[i+1];
-            data[i+1]= temp;
-            needswap= true;
+    for(int i=0; i< data.length; i++){
+      for(int j=0; j< data.length-1-i; j++){
+        if(data[j]> data[j+1]){
+            temp = data[j];
+            data[j]= data[j+1];
+            data[j+1]= temp;
         }
       }
     }
-  }
 }
+
+public static void selectionSort(int[] data){
+  int temp;
+  for(int i=0; i< data.length-1; i++){
+      for(int j=0; j< data.length-1; j++){
+          int smallest= data[i];
+              if(smallest > data[j]){
+                smallest= data[j];
+                data[j]= data[i];
+                data[i] = smallest;
+              }
+            }
+    }
+}
+public static void insertionSort(int[] data){
+  int temp;
+  for(int i=1; i< data.length; i++){ //go through each part of the list
+            temp= data[i];
+            int counter= i;
+            while(counter >0 && data[counter-1] > temp){
+              data[counter] = data[counter-1];
+              counter--;
+          }
+          data[counter]= temp;
+        }
+      }
+    }
