84c82e04a3e6b982b0a973c82d0fdb1a4b4ac7d7
sging20 <sging20@stuy.edu>
2020-12-22 22:25:33 -0500

    woowoo

---
+++ Driver.java
@@ -0,0 +1,85 @@
+import java.util.Arrays;
+import java.util.Random;
+public class Driver{
+  public static void main(String[]args){
+    if(args.length < 2){
+      System.out.println("usage: java Driver SIZE ALGO [STYLE]");
+      System.out.println("supported ALGO: bubble,selection,insertion,test");
+      System.out.println("supported STYLE: random,equal,sorted,reversed");
+    }else{
+
+
+      int[] randData = new int[Integer.parseInt(args[0])];
+
+      //usage: java Driver SIZE ALGO [STYLE]
+// supported ALGO: bubble,selection,insertion,test
+// supported STYLE: random,equal,sorted,reversed
+// //args 0 must be size of the Array
+    //args 1 must be "bubble" or "test" (unless you add more)
+
+              Random rand = new Random();
+      if(args.length < 3 || args[2].equals("random")){
+        for(int i=0; i< randData.length; i++){
+	     randData[i] = rand.nextInt() % 1000;
+      }
+      }else if(args[2].equals("equal")){
+       int randomnumber= rand.nextInt() % 1000;
+       for(int i=0; i< randData.length; i++){
+         randData[i]= randomnumber;
+       }
+      }else if(args[2].equals("sorted")){
+        int rando= rand.nextInt() % 1000;
+        for(int i=1; i< randData.length; i++){
+          int adder= (int) Math.random() * 99;
+          randData[i]= randData[i-1] - adder;
+        }
+      }else if(args[2].equals("reversed")){
+         int randomnum= rand.nextInt() % 1000;
+         for(int i= 1; i< randData.length-1; i++){
+           randData[i] = randData[i-1] - ((int) Math.random() * 120);
+         }
+       }
+
+      if(args[1].equals("bubble")){
+        Sorts.bubbleSort(randData);
+      }
+      else if(args[1].equals("selection")){
+        Sorts.selectionSort(randData);
+      }
+      else if(args[1].equals("insertion")){
+        Sorts.insertionSort(randData);
+      }
+      //TEST MODE! This lets you see if your algorithm fails
+      //for a particular type of array, or size of array.
+      //This is not meant to be a timed test...
+      else if(args[1].equals("test")){
+
+        int[] randDataBubble = Arrays.copyOf(randData,randData.length);
+        int[] randDataSelection = Arrays.copyOf(randData,randData.length);
+        int[] randDataInsertion = Arrays.copyOf(randData,randData.length);
+        Arrays.sort(randData);
+        Sorts.bubbleSort(randDataBubble);
+        Sorts.selectionSort(randDataSelection);
+        Sorts.insertionSort(randDataInsertion);
+
+        if( Arrays.equals(randData,randDataBubble)){
+          System.out.println("Bubble Correct!");
+        }else{
+         System.out.println("Bubble Not Correct!!!!!!!!!11oneeleven");
+        }
+
+        if( Arrays.equals(randData,randDataSelection)){
+          System.out.println("Selection Correct!");
+        }else{
+         System.out.println("Selection Not Correct!!!!!!!!!11oneeleven");
+        }
+
+        if( Arrays.equals(randData,randDataInsertion)){
+          System.out.println("Insertion Correct!");
+        }else{
+         System.out.println("Insertion Not Correct!!!!!!!!!11oneeleven");
+        }
+      }
+    }
+  }
+}
